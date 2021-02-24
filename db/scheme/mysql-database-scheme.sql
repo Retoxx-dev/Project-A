71d956af6b2c07c6d29002ef3b2b037ffc4cbc5b
Kacper Dziedzic <kacper.dziedzic@microsoft.wsei.edu.pl>
2021-02-24 11:56:56 +0100

    Initial docker implemenation

---
+++ db/scheme/mysql-database-scheme.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE login;
+GRANT ALL PRIVILEGES ON login.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON login.* TO 'root'@'localhost';
+USE login;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `user_type` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+COMMIT;
